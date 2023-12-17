35e1cb097c5607fced495e464dd011c0ad46991c
Sean P <spatel280@gmail.com>
2023-12-17 15:46:03 -0800

    Updated code

--- customer_banking.py
+++ customer_banking.py
@@ -8,7 +8,9 @@ def main():
     It displays the interest earned on the savings and CD accounts and updates the balances.
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
-    # ADD YOUR CODE HERE
+    savings_balance = float(input("Enter your savings balance: "))
+    savings_rate = float(input("Enter your interest rate: "))
+    savings_months = int(input("Enter your number of months: "))
 
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
